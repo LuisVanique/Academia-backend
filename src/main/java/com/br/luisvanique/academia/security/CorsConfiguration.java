829f04665f9e7c6f75d302822541b3b11d1c1796
LuisVanique <cunhagustavo142@gmail.com>
2024-08-10 10:42:24 -0300

    resolve cors

--- src/main/java/com/br/luisvanique/academia/security/CorsConfiguration.java
+++ src/main/java/com/br/luisvanique/academia/security/CorsConfiguration.java
@@ -8,9 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfiguration implements WebMvcConfigurer {
 	
 	@Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("teste") // ALTERAR PARA URL DO FRONT END
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
-    }
+	public void addCorsMappings(CorsRegistry registry) {
+	    registry.addMapping("/login")
+	            .allowedOrigins("*") // Permite todas as origens
+	            .allowedMethods("GET", "POST", "OPTIONS");
+
+	    registry.addMapping("/instrutor")
+	            .allowedOrigins("*") // Permite todas as origens
+	            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+	}
 }
