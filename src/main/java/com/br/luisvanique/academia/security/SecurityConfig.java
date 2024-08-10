c77e31ad544f83bacf565206d574a6669dc18c23
LuisVanique <cunhagustavo142@gmail.com>
2024-08-10 10:55:51 -0300

    resolve cors

--- src/main/java/com/br/luisvanique/academia/security/SecurityConfig.java
+++ src/main/java/com/br/luisvanique/academia/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.br.luisvanique.academia.security;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -11,6 +13,10 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -24,17 +30,27 @@ public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-
-		httpSecurity.cors(cors -> cors.disable());
-		httpSecurity.csrf(csrf -> csrf.disable())
-				.sessionManagement(
-						sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-				.authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
-						.requestMatchers(HttpMethod.POST, "/login").permitAll()
-						.requestMatchers(HttpMethod.POST, "/instrutor").permitAll().anyRequest().authenticated()
-
-				).addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
-		return httpSecurity.build();
+	    return httpSecurity.csrf(csrf -> csrf.disable())
+	            .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+	            .cors(withDefaults()) // Inclua essa linha para habilitar CORS
+	            .authorizeHttpRequests(authorizeHttpRequests -> 
+	                    authorizeHttpRequests.requestMatchers(HttpMethod.POST, "/login").permitAll()
+	                    .requestMatchers(HttpMethod.POST, "/instrutor").permitAll()
+	                    .anyRequest().authenticated()
+	            )
+	            .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+	            .build();
+	}
+	
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+	    CorsConfiguration configuration = new CorsConfiguration();
+	    configuration.setAllowedOrigins(Arrays.asList("*")); // Pode ajustar conforme necessário
+	    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+	    configuration.setAllowedHeaders(Arrays.asList("*"));
+	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    source.registerCorsConfiguration("/**", configuration);
+	    return source;
 	}
 
 	@Bean
